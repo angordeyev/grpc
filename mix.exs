3547eccf5968a2a9d31a6dc3eea0a445269c67c0
angordeyev <angordeyev@gmail.com>
2022-02-13 23:05:23 +0300

    updates

--- mix.exs
+++ mix.exs
@@ -37,8 +37,8 @@ defmodule GRPC.Mixfile do
   defp deps do
     [
       {:protobuf, "~> 0.5"},
-      {:cowboy, "~> 2.7"},
-      {:gun, "~> 2.0.0", hex: :grpc_gun},
+      {:cowboy, "~> 2.9"},
+      {:gun, "~> 2.0.0-rc.2"},
       # 2.9.0 fixes some important bugs, so it's better to use ~> 2.9.0
       # {:cowlib, "~> 2.9.0", override: true},
       {:ex_doc, "~> 0.23", only: :dev},
